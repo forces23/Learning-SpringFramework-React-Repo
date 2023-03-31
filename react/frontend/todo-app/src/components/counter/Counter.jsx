33d8962aa4e1d8a187924a6d33fde40c7dde32af
Bobby <bobby.lawson17@gmail.com>
2023-03-31 10:03:48 -0500

    updating React Project

--- react/frontend/todo-app/src/components/counter/Counter.jsx
+++ react/frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,21 +1,50 @@
+import { useState } from 'react'
+import "./Counter.css"
+
 export default function Counter() {
 
-    const buttonStyle = {fontSize:"30px"}
+    //const state = useState(0)
+    const [count, setCount] = useState(0)
 
     function incrementCounterFunction(){
-        console.log('increment clicked')
+        console.log('!!! increment clicked !!!') 
+        setCount(count + 1)
+        //console.log("state: " + state)
     }
 
+    function decrementCounterFunction(){
+        console.log('!!! decrement clicked !!!')
+        setCount(count - 1)
+    }
 
     return (
         <div className="counter">
-            <span className="count">0</span>
             <div>
                 <button className="counterButton"
-                        onClick={incrementCounterFunction}
-                        style={buttonStyle}
-                        
+                        onClick={incrementCounterFunction}   
                 >+1</button>
+                <button className="counterButton"
+                        onClick={decrementCounterFunction}
+                >-1</button>
+                <br/>
+                <button className="counterButton"
+                        onClick={""}   
+                >+2</button>
+                <button className="counterButton"
+                        onClick={""}
+                >-2</button>
+                <br/>
+                <button className="counterButton"
+                        onClick={""}   
+                >+5</button>
+                <button className="counterButton"
+                        onClick={""}
+                >-5</button>
+                <br/>   
+                <div className="count">{count}</div>
+                <button className="counterButton"
+                        onClick={""}
+                >Reset</button>
             </div>
         </div>
     )
